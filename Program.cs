04828bda316cbc1013c647caecbebe3501022816
Rhuan Martins de Souza <rhuanshura@live.com>
2023-10-17 14:52:03 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "11 976922222", modelo: "Tijolão", imei:"522900047623000", memoria: 2);
+nokia.Ligar();
+nokia.InstalarAplicativo("ZapZap");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "11 976933333", modelo: "Pro Max 15", imei:"111900047623000", memoria: 62);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
